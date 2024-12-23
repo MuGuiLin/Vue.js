2792b8d863013d1e31cb31e2caf5338c33c0c3b4
muguilin <muguilin@foxmail.com>
2024-12-23 11:17:03 +0800

    update

--- Vue3/vite4+vue3+tsx5+antdv4/src/hooks/useTheme.ts
+++ Vue3/vite4+vue3+tsx5+antdv4/src/hooks/useTheme.ts
@@ -1,14 +1,30 @@
 import { ref, watchEffect, getCurrentInstance } from 'vue';
-type Theme = 'light' | 'dark';
+type Theme = 'light' | 'dark' | 'os';
 const STORE_KEY = '__hteme__';
 
+// 获取本地主题
 const theme = ref<Theme>(localStorage.getItem(STORE_KEY) as Theme || 'dark');
+
+// 获取系统主题
+const match = window.matchMedia('(prefers-color-scheme: dark)')
+
+const setOsTheme = () => {
+    theme.value = match.matches ? 'dark' : 'light';
+    document.documentElement.setAttribute('data-theme', theme.value);
+};
 
 // 监听theme变量值变化
 watchEffect(() => {
-    // document.documentElement.dataset.theme = theme.value;
-    document.documentElement.setAttribute('data-theme', theme.value);
     localStorage.setItem(STORE_KEY, theme.value);
+    if ('os' === theme.value) {
+        setOsTheme();
+        // 注册监听系统主题变化
+        match.addEventListener('change', setOsTheme);
+    } else {
+        match.removeEventListener('change', setOsTheme);
+        // document.documentElement.dataset.theme = theme.value;
+        document.documentElement.setAttribute('data-theme', theme.value);
+    }
 });
 
 export function useTheme() {
